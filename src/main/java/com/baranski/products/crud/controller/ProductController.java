b6feecf1b6cf30625d5b704250995091a35f3d31
MarcinBarański <mbaranski92@gmail.com>
2021-09-16 21:00:05 +0200

    Feature: new product adding - works. Added new product: webpage with form. Added method in controller for saving new product retrieved from form to DB.

--- src/main/java/com/baranski/products/crud/controller/ProductController.java
+++ src/main/java/com/baranski/products/crud/controller/ProductController.java
@@ -5,7 +5,9 @@ import com.baranski.products.crud.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
@@ -20,6 +22,19 @@ public class ProductController {
         List<Product> products = productService.listAll();
         model.addAttribute("listProducts",products);
         return "index";
+    }
+
+    @RequestMapping("/new")
+    public String showNewProductPage(Model model){
+        Product product = new Product();
+        model.addAttribute("product",product);
+        return "newProduct";
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String saveProduct(@ModelAttribute("product") Product product){
+        productService.addProduct(product);
+        return "redirect:/";
     }
 
 
